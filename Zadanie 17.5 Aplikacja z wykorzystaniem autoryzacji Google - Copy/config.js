a4ef42880bc790b1134a49b44910d90ea84c1bc4
Maciej Kubas <maciekkubas@gmail.com>
2018-05-20 12:35:59 +0200

    task-completed

---
+++ Zadanie 17.5 Aplikacja z wykorzystaniem autoryzacji Google - Copy/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '571126080168-60tg6ffcn2fso413ghkajatrvo1u0n3f.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '8NTrzI9GxAX6Thz8iN_Ptlk8',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
