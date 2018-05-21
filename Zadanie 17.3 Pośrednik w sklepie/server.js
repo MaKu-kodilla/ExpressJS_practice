18f9340dab4a1024a8dccd0a717a6310d4717a5e
Maciej Kubas <maciekkubas@gmail.com>
2018-05-21 18:50:15 +0200

    modified server.js

--- Zadanie 17.3 Pośrednik w sklepie/server.js
+++ Zadanie 17.3 Pośrednik w sklepie/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use ('/store', function(req, res, next) {
+app.use('/store', function(req, res, next) {
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
 });
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-app.get ('/store', function(req, res) {
+app.get('/store', function(req, res) {
     res.send('To jest sklep');
 });
 
